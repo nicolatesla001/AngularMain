3d1eeb25a67ba3cf7a3dbda9ef7ce25a28243a4d
nicolatesla001 <makeyoumove96@gmail.com>
2022-05-17 22:44:54 +0300

    les1-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,63 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
+import {Route, RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UserComponent} from './components/user/user.component';
+import {UsersComponent} from './components/users/users.component';
+import {PostDetailsComponent} from './components/post-details/post-details.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {HeaderComponent} from './components/header/header.component';
+import {PostComponent} from './components/post/post.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {MainLayoutComponent} from './layouts/main-layout/main-layout.component';
+import {UserDetailsComponent} from './components/user-details/user-details.component';
+import {CommentDetailsComponent} from './components/comment-details/comment-details.component';
+
+
+const routes: Route[] = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: 'users', pathMatch: 'full'},
+      {
+        path: 'users', component: UsersComponent, children: [
+          {path:':id', component:UserDetailsComponent}
+        ]
+      },
+      {
+        path: 'posts', component: PostsComponent, children: [
+          {path:':id',component:PostDetailsComponent}
+        ]
+      },
+      {path: 'comments', component: CommentsComponent, children: []}
+    ]
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent,
+    CommentComponent,
+    CommentsComponent,
+    HeaderComponent,
+    PostComponent,
+    PostsComponent,
+    MainLayoutComponent,
+    UserDetailsComponent,
+    PostDetailsComponent,
+    CommentDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
